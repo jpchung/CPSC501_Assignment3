b34a95ee3ed7d2abad33a96689273d92e825a873
jpchung <jpchungjpc@gmail.com>
2017-11-06 20:45:39 -0700

    initial commit, added jdom libary and empty Serializer/Deserializer classes

---
+++ src/Deserializer.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Owner on 2017-11-06.
+ */
+
+import java.lang.reflect.*;
+import org.jdom.*;
+public class Deserializer {
+
+    //public Object deserialize(org.jdom.Document document)
+}
---
+++ src/Serializer.java
@@ -0,0 +1,11 @@
+/**
+ * Serializer class for Sender program
+ * @author Johnny Chung
+ */
+
+import java.lang.reflect.*;
+import org.jdom.*;
+public class Serializer {
+
+    //public org.jdom.Document serialize(Object obj)
+}
